8aa571bb9ae28e9afb87b5c1b3262a2c9bf23b2e
Himangmyid <himangbd181021@gmail.com>
2025-03-07 01:45:02 +0800

    First commit

---
+++ public/link.js
@@ -0,0 +1,33 @@
+const links = {
+    website: "https://s.id/himang",
+    mycoffee: "https://trakteer.id/himang/tip",
+    facebook: "https://www.facebook.com/himangbd181021/",
+    instagram: "https://instagram.com/himang_dg",
+    twitter: "https://x.com/himangmyid",
+    youtube: "https://www.youtube.com/channel/UCX8aSUkYR0tAW3md1JFmhnQ?sub_confirmation=1",
+    github: "https://github.com/himangmyid",
+    tiktok: "https://tiktok.com/@himang_dg",
+    telegram: "/",
+    pinterest: "/",
+    linkedin: "https://s.id/linkedin-himang",
+    spotify: "/",
+    discord: "https://discord.gg/MP2pSKjXSB",
+    dribbble: "/",
+    medium: "/",
+    paypal: "https://paypal.me/DogGhozt",
+    twitch: "https://www.twitch.tv/himanghub",
+    whatsapp: "/",
+    threads: "https://www.threads.net/@himang_dg",
+    marketplace: "/",
+  };
+  
+  // Auto-inject links ke dalam HTML
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("[data-link]").forEach((el) => {
+      const key = el.getAttribute("data-link");
+      if (links[key]) {
+        el.href = links[key];
+      }
+    });
+  });
+  
---
+++ public/script.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.remove('hidden');
+    } else {
+        navbar.classList.add('hidden');
+    }
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("gridToggle");
+    const container = document.getElementById("link-container");
+
+    toggle.addEventListener("change", function () {
+        if (this.checked) {
+            container.classList.add("grid-cols-2");
+            container.classList.remove("grid-cols-1");
+        } else {
+            container.classList.remove("grid-cols-2");
+            container.classList.add("grid-cols-1"); // Fix here
+        }
+    });
+});
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const snowContainer = document.querySelector(".snow-container");
+
+    function createSnowflake() {
+      const snowflake = document.createElement("div");
+      snowflake.classList.add("snowflake");
+      snowflake.innerHTML = "❄";
+      snowflake.style.left = Math.random() * 100 + "vw";
+      snowflake.style.animationDuration = Math.random() * 3 + 2 + "s";
+      snowflake.style.fontSize = Math.random() * 10 + 10 + "px";
+
+      snowContainer.appendChild(snowflake);
+
+      setTimeout(() => {
+        snowflake.remove();
+      }, 5000);
+    }
+
+    setInterval(createSnowflake, 200);
+  });
+
